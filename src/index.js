efe95348acbe06783215da8c06d9441f47e1b50c
mkrtum <mkrtum.abrahamyan@icloud.com>
2025-01-09 15:49:59 -0800

    change pixel id

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import initFacebookPixel from './utils/metaPixel/facebookPixel';
 
-const PIXEL_ID = '1393362078221567'; // Replace with your Pixel ID
+const PIXEL_ID = '1393362078221567'; // Replace with your Pixel ID 1
 initFacebookPixel(PIXEL_ID);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
