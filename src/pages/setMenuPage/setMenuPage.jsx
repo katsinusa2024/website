be367ce3e7179294cda54714d2b325007586aa94
mkrtum <mktum.abrahamyan@icloud.com>
2025-09-24 19:33:35 -0700

    delete 110 setmenu

--- src/pages/setMenuPage/setMenuPage.jsx
+++ src/pages/setMenuPage/setMenuPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './setMenuPage.scss';
-import setMenu1 from '../../assets/images/setMenu/setMenu110.jpg';
 import setMenu2 from '../../assets/images/setMenu/setMenu125.jpg';
 import setMenu3 from '../../assets/images/setMenu/setMenu150.jpg';
 
@@ -9,7 +8,6 @@ const SetMenuPage = () => {
     <div className="setMenuPage pageWrapper">
       <div className="container">
         <h1 className="mb-5 text-uppercase event-name">Set Menu</h1>
-        <img src={setMenu1} alt="Menu $125" className="img-fluid mb-5" />
         <img src={setMenu2} alt="Menu $150" className="img-fluid mb-5" />
         <img src={setMenu3} alt="Menu $150" className="img-fluid" />
       </div>
